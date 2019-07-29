dcb3458901c2b1507585f17dfdaee43725d81834
xiechun520 <50307925+xiechun520@users.noreply.github.com>
2019-07-29 14:27:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,2 @@
 import Vue from 'vue'
 import App from './App'
-
-import store from './store'
-
-import axios from './Api'
-import baseUrl from './Api/baseUrl'
-
-Vue.prototype.$axios =axios;
-Vue.prototype.$baseUrl =baseUrl;
-
-
-import cuCustom from './colorui/components/cu-custom.vue'
-Vue.component('cu-custom',cuCustom)
-
-Vue.config.productionTip = false
-
-Vue.prototype.$store = store
-
-App.mpType = 'app'
-
-const app = new Vue({
-    store,
-    ...App
-})
-app.$mount()
